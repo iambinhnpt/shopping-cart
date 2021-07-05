7ad704c47963f94ddeaaa2b49d8b81c37821efe1
DESKTOP-IH2488M\npthi <Binh Pro>
2021-07-06 02:23:46 +0700

    create product component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import React, { Component } from "react";
+import Products from "./components/Products";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header className="header">
-        <a href="/">React shopping cart</a>
-      </header>
-      <main>
-        product list
-      </main>
-      <footer>All right is reserved</footer>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header className="header">
+          <a href="/">React shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
 }
-
-export default App;
